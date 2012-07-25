f891c50e3ca2603baa046b76eebbaac11b5f49af
Karl Heyes <karl@xiph.org>
2012-07-25 01:47:45 +0100

    prevent source client bouncing across workers in quiet cases

--- src/source.c
+++ src/source.c
@@ -2227,7 +2227,7 @@ static int source_change_worker (source_t *source, client_t *client)
     if (worker && worker != client->worker)
     {
         long diff = this_worker->count - worker->count;
-        if (diff > 35 || diff > (source->listeners>>1))
+        if (diff > 35 || (diff > (source->listeners>>1) + 3))
         {
             this_worker->move_allocations--;
             thread_rwlock_unlock (&source->lock);
